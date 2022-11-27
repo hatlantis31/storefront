f67d81471e03d29c3345fac4f399e0bc8f2d0d96
Malo BRIAND <malo.briand@nikon.com>
2022-11-27 22:09:04 +0000

    exercise 1

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from store.models import Product
+from store.models import Customer
+from store.models import Collection
+from store.models import Order
+from store.models import OrderItem
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+    Customer_with_com = Customer.objects.filter(email__iendswith='.com')
+
+    col_featured = Collection.objects.filter(featured_product_id__isnull=True)
+
+    product_low_inventory = Product.objects.filter(inventory__lt=20)
+
+    order_by1 = Order.objects.filter(customer_id=1)
+
+    item_in_col3 = OrderItem.objects.filter(product__collection_id=3)
+
+
+    return render(request, 'hello.html', {'name': 'Malo',
+                                          'product_with_com': list(Customer_with_com),
+                                          'col_featured': list(col_featured),
+                                          'product_low_inventory':list(product_low_inventory),
+                                          'order_by1':list(order_by1),
+                                          'item_in_col3':list(item_in_col3),})
